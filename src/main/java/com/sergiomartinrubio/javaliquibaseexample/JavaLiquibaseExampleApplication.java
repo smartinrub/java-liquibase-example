181e18ee0b9cdb962c9d3ddb180e5756ce7609fd
Sergio Martin Rubio <econsergio@gmail.com>
2021-04-23 11:16:49 +0200

    Add files

---
+++ src/main/java/com/sergiomartinrubio/javaliquibaseexample/JavaLiquibaseExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sergiomartinrubio.javaliquibaseexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLiquibaseExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLiquibaseExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergiomartinrubio/javaliquibaseexample/JavaLiquibaseExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiomartinrubio.javaliquibaseexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLiquibaseExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
